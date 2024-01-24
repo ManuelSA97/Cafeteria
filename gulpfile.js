9087c177412fc3a05a4342abb9bf89a6f544d6b1
Manuel Sandi <manuelsandi97@hotmail.com>
2024-01-24 17:38:14 -0600

    conversion de imagenes, llamada de funciones gulpfile, instalacion de dependencias

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 const {src, dest, watch, series, parallel } = require('gulp');
+
+// CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 
 function css(done){
@@ -15,16 +22,35 @@ function css(done){
     done();
 }
 
-function dev(){
-    watch('src/scss/**/*.scss', css);
+function imagenes(){
+    return src('src/img/**/*')
+    .pipe(imagemin({optimizationLevel: 3}))
+    .pipe(dest('build/img'));
+    
 }
 
-function tareaDefault(){
-    console.log('soy la tarea def');
+function versionWebp(){
+    return src('src/img/**/*.{png,jpg}')
+    .pipe(webp())
+    .pipe(dest('build/img'));
+}
+
+function versionAvif(){
+    return src('src/img/**/*.{png,jpg}')
+    .pipe(avif())
+    .pipe(dest('build/img'));
+}
+
+function dev(){
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes); //por si se agregan nuevas imagenes
 }
 
 exports.css = css;
 exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
 exports.default = series(css, dev);
 
 //series - Se inicia una tarea y hasta que finaliza se inicia la siguiente (Dejar a tarea con watch hasta el final)
